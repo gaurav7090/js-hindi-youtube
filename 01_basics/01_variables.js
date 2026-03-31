dbf9bd58ffae005340ed4f68602ef916125e7ff5
Gaurav Kumar <gk76095@gmail.com>
2026-03-31 21:27:33 +0000

    container config done

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,12 @@
+const studentId = 1234
+let studentEmail = "gk76095@gmail.com"
+var studentPassword = "123456"
+studentCity = "Hajipur"
+let ramkr
+
+
+studentEmail = "ramkrmishra@gmail.com"
+studentPassword = "9090909"
+studentCity = "Patna"
+console.log(studentId);
+console.log([studentEmail, studentPassword, studentCity, ramkr]);
